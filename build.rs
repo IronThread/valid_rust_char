1020d7a32c00c8f19b0c12457e3cf6723a6b82a9
Santino <74435533+IronThread@users.noreply.github.com>
2020-12-31 20:10:27 -0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
-use std::fs::File;
-use std::error::Error;
 use std::char;
+use std::error::Error;
+use std::fs::File;
 use std::io::Write;
+use std::iter::FusedIterator;
+use std::ops::RangeInclusive;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut it = invalid_rust_table().map(u32::from);
@@ -16,25 +18,81 @@ fn main() -> Result<(), Box<dyn Error>> {
         } else {
             vec.push(lb..=hb);
             lb = c;
-            hb = c;            
+            hb = c;
         }
     }
 
     let mut f = File::create("src/consts.rs")?;
 
-    writeln!(f, "/// A table with all ranges of unicode scalar values of characters that are not")?;
+    writeln!(
+        f,
+        "/// A table with all ranges of unicode scalar values of characters that are not"
+    )?;
     writeln!(f, "/// valid in Rust.")?;
-    writeln!(f, "pub const RUST_INVALID_TABLE: [core::ops::RangeInclusive<u32>; {}] = {:#?};", vec.len(), vec)?;
-    Ok(())   
+    writeln!(
+        f,
+        "pub const RUST_INVALID_TABLE: [core::ops::RangeInclusive<u32>; {}] = {:#?};",
+        vec.len(),
+        vec
+    )?;
+    Ok(())
 }
 
 fn invalid_rust_table() -> impl Iterator<Item = char> {
-    all_unicode().filter(is_not_rust_char)
+    AllUnicode::new().filter(is_not_rust_char)
 }
 
-fn all_unicode() -> impl Iterator<Item = char> {
-    (0..=char::MAX as u32).filter_map(char::from_u32)
+struct AllUnicode(RangeInclusive<u32>, RangeInclusive<u32>);
+
+impl AllUnicode {
+    #[inline]
+    pub fn new() -> Self {
+        Self(0..=0xD800 - 1, 0xDFFF + 1..=char::MAX as u32)
+    }
 }
+
+impl Default for AllUnicode {
+    #[inline]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Iterator for AllUnicode {
+    type Item = char;
+
+    #[inline]
+    fn next(&mut self) -> Option<char> {
+        self.0
+            .next()
+            .or_else(|| self.1.next())
+            .and_then(char::from_u32)
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len(), Some(self.len()))
+    }
+}
+
+impl DoubleEndedIterator for AllUnicode {
+    #[inline]
+    fn next_back(&mut self) -> Option<char> {
+        self.1
+            .next_back()
+            .or_else(|| self.0.next_back())
+            .and_then(char::from_u32)
+    }
+}
+
+impl ExactSizeIterator for AllUnicode {
+    #[inline]
+    fn len(&self) -> usize {
+        self.0.size_hint().0 + self.1.size_hint().0
+    }
+}
+
+impl FusedIterator for AllUnicode {}
 
 fn is_not_rust_char(c: &char) -> bool {
     let mut buf = [0; 4];
@@ -52,6 +110,9 @@ fn is_not_rust_char(c: &char) -> bool {
         '"' => false,
         '_' => false,
         '\\' => false,
-        _ => c.encode_utf8(&mut buf).parse::<proc_macro2::TokenStream>().is_err()
+        _ => c
+            .encode_utf8(&mut buf)
+            .parse::<proc_macro2::TokenStream>()
+            .is_err(),
     }
 }
